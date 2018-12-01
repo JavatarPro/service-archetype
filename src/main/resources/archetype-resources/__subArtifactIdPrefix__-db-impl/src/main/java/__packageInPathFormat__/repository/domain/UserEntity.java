cc3092f8d1b4e4dfb4fa238faed379c66e4a98db
Serhii Petrychenko <spe@adorsys.com.ua>
2018-12-02 00:04:52 +0200

    added sequence generator

--- src/main/resources/archetype-resources/__subArtifactIdPrefix__-db-impl/src/main/java/__packageInPathFormat__/repository/domain/UserEntity.java
+++ src/main/resources/archetype-resources/__subArtifactIdPrefix__-db-impl/src/main/java/__packageInPathFormat__/repository/domain/UserEntity.java
@@ -7,7 +7,7 @@ import java.util.Objects;
 @Table(name = "users")
 public class UserEntity {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(unique = true, updatable = false)
