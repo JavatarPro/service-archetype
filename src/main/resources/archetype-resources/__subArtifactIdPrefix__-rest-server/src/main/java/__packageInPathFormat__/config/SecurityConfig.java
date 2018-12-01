6d04a3c39025bd93aede115b70ba7537190f739d
Serhii Petrychenko <serhii.petrychenko@javatar.pro>
2018-12-02 00:46:07 +0200

    moved h2 to file storage usage

--- src/main/resources/archetype-resources/__subArtifactIdPrefix__-rest-server/src/main/java/__packageInPathFormat__/config/SecurityConfig.java
+++ src/main/resources/archetype-resources/__subArtifactIdPrefix__-rest-server/src/main/java/__packageInPathFormat__/config/SecurityConfig.java
@@ -12,5 +12,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
+        http.headers().frameOptions().disable();
     }
 }
